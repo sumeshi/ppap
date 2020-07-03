1e9d16bc4c0e4b534fbae9d71804e0b388655b02
sumeshi <j15322sn@gmail.com>
2020-07-03 16:38:35 +0900

    feat: remove tempdir when exit with err

--- src/ppap.py
+++ src/ppap.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-import time
 import shutil
 import argparse
+import traceback
 from typing import List
 from pathlib import Path
 from datetime import datetime
@@ -33,38 +33,41 @@ def decrypt_pass(passphrase: bytes, key: RsaKey) -> str:
     return private_cipher.decrypt(passphrase).decode()
 
 
-def encrypt(filepath_list: List[str], now: str):
+def encrypt(filepath_list: List[str], key_path: str, now: str):
     # generate encrypted_key
     zipkey = get_random_bytes(128).hex()
-    crypted = encrypt_pass(zipkey, load_key(Path("./keys/test.pub")))
+    crypted = encrypt_pass(zipkey, load_key(Path(key_path)))
 
-    # create tarball
-    tempdir = Path(f"./temp-ppap-{get_now()}").resolve()
-    tarball_path = compress_contents(filepath_list, tempdir)
+    try:
+        # create tarball
+        tempdir = Path(f"./temp-ppap-{get_now()}").resolve()
+        tarball_path = compress_contents(filepath_list, tempdir)
 
-    # zip with passphrase
-    pyminizip.compress(
-        str(tarball_path),
-        None,
-        str(tempdir / Path("ppap_encrypted_contents.zip")),
-        zipkey,
-        4,
-    )
+        # zip with passphrase
+        pyminizip.compress(
+            str(tarball_path),
+            None,
+            str(tempdir / Path("ppap_encrypted_contents.zip")),
+            zipkey,
+            4,
+        )
 
-    # delete original contents
-    tarball_path.unlink()
+        # delete original contents
+        tarball_path.unlink()
 
-    # copy encrypted key
-    key_path = tempdir / Path(".encrypted_key")
-    key_path.write_text(crypted.hex())
+        # copy encrypted key
+        encrypted_key_path = tempdir / Path(".encrypted_key")
+        encrypted_key_path.write_text(crypted.hex())
 
-    # compress all files
-    shutil.make_archive(f"./ppap-{get_now()}", "zip", root_dir=tempdir)
+        # compress all files
+        shutil.make_archive(f"./ppap-{get_now()}", "zip", root_dir=tempdir)
 
-    # remoe tempdir
-    shutil.rmtree(tempdir)
+    except Exception:
+        traceback.print_exc()
 
-    print("completed")
+    finally:
+        # remove tempdir
+        shutil.rmtree(tempdir)
 
 
 def compress_contents(filepath_list: List[str], tempdir: Path) -> Path:
@@ -97,46 +100,51 @@ def compress_contents(filepath_list: List[str], tempdir: Path) -> Path:
     return tarball_path.with_suffix(".tar")
 
 
-def decrypt(file_path: str):
-    # unpack
-    tempdir = Path(f"./temp-ppap-{get_now()}").resolve()
-    shutil.unpack_archive(file_path, str(tempdir), "zip")
+def decrypt(file_path: str, key_path: str, passphrase: str):
+    try:
+        # unpack
+        tempdir = Path(f"./temp-ppap-{get_now()}").resolve()
+        shutil.unpack_archive(file_path, str(tempdir), "zip")
 
-    # decrypt key
-    encrypted_key = tempdir / Path(".encrypted_key")
-    decrypted_key = decrypt_pass(
-        bytes.fromhex(encrypted_key.read_text()), load_key(Path("./keys/test"))
-    )
+        # decrypt key
+        encrypted_key = tempdir / Path(".encrypted_key")
+        decrypted_key = decrypt_pass(
+            bytes.fromhex(encrypted_key.read_text()),
+            load_key(Path(key_path), passphrase=passphrase),
+        )
 
-    # decrypt file
-    encrypted_contents = tempdir / Path("ppap_encrypted_contents.zip")
-    pyminizip.uncompress(
-        str(encrypted_contents), decrypted_key, str(tempdir), int(),
-    )
+        # decrypt file
+        encrypted_contents = tempdir / Path("ppap_encrypted_contents.zip")
+        pyminizip.uncompress(
+            str(encrypted_contents), decrypted_key, str(tempdir), int(),
+        )
 
-    decrypted_contents = tempdir / Path("ppap_contents.tar")
-    dest = Path(tempdir.parent, f"ppap-{get_now()}").resolve()
-    shutil.unpack_archive(
-        str(decrypted_contents), dest, "tar",
-    )
+        decrypted_contents = tempdir / Path("ppap_contents.tar")
+        dest = Path(tempdir.parent, f"ppap-{get_now()}").resolve()
+        shutil.unpack_archive(
+            str(decrypted_contents), dest, "tar",
+        )
 
-    # delete tempfiles
-    shutil.rmtree(tempdir)
+    except Exception:
+        traceback.print_exc()
+
+    finally:
+        # remove tempdir
+        shutil.rmtree(tempdir)
 
 
 def ppap():
     parser = argparse.ArgumentParser(description="PPAP")
-    parser.add_argument("filepath_list", nargs="+", help="")
-    parser.add_argument("--out", "-o", type=str, default="", help="")
+    parser.add_argument("filepath_list", nargs="+", help="Input Path")
+    parser.add_argument("--out", "-o", type=str, default="", help="Destination Path")
+    parser.add_argument("--key", help="Public/Private key file(RSA).")
 
     # encrypt
     parser.add_argument("--encrypt", "-e", action="store_true", help="")
-    parser.add_argument("--passphrase", "-p", type=str, default="", help="")
-    parser.add_argument("--key", help="")
 
     # decrypt
     parser.add_argument("--decrypt", "-d", action="store_true", help="")
-    parser.add_argument("--user", help="")
+    parser.add_argument("--passphrase", "-p", type=str, default="", help="")
     args = parser.parse_args()
 
     try:
@@ -149,14 +157,17 @@ def ppap():
             )
 
         if args.encrypt:
-            encrypt(args.filepath_list, now=get_now())
+            encrypt(args.filepath_list, args.key, now=get_now())
         else:
-            for filepath in args.filepath_list:
-                decrypt(filepath)
+            if len(args.filepath_list) == 1:
+                decrypt(args.filepath_list[0], args.key, passphrase=args.passphrase)
+            else:
+                raise Exception(
+                    "Exception: Only one file can be selected for decryption."
+                )
 
     except Exception as e:
         print(e)
-        # traceback.print_exc()
 
 
 if __name__ == "__main__":
